65624f558d90f1ceb7f25aa61eb7f7639e00b825
newtonkiragu <karanunewton4@gmail.com>
2018-06-11 20:30:30 +0300

    updated view templates for landing and movie routes

--- app/views.py
+++ app/views.py
@@ -7,12 +7,13 @@ def index():
     '''
     view root page function that returns index and it\'s data
     '''
-    message = 'Hello! I am a variable!'
-    return render_template('index.html', message = message)
+    title = 'Home - Welcome to the best movie review website online'
+    return render_template('index.html', title = title)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
     '''
     view movie page function that returns movies and all it\'s data
     '''
+    title = 'Home - Welcome to the best movie review website online'
     return render_template('movie.html', id = movie_id)
