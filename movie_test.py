ea7187042877a8deeb73a1e1b9ca4823a81ea619
newtonkiragu <karanunewton4@gmail.com>
2018-06-11 23:20:28 +0300

    wrote test initialisers for movie model

--- movie_test.py
+++ movie_test.py
@@ -1,5 +1,21 @@
 import unittest
-from ..models import movie
+from .models import movie
 Movie = movie.Movie
 
-class
+class MovieTest(unittest.TestCase):
+    '''
+    Test class to test the behaviour of the movie class
+    '''
+
+    def setUp(self):
+        '''
+        Setup method that will run before every test
+        '''
+
+        self.new_movie = Movie(1234,'Python must be crazy','A thrilling new python series','http://lorempixel.com/400/200',8.5,129993)
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie,Movie))
+
+if __name__ == '__main__':
+    unittest.main()
