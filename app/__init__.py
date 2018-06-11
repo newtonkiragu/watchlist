2b787553fd7d3f354b1c7d20a687c84d6ffd4e64
Newton Karanu <monster@localhost.localdomain>
2018-06-11 20:20:23 +0300

    added configuration for production and development modes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from .config import DevConfig
 
 # initialising the application
 app = Flask(__name__)
+
+# setting up configuration
+app.config.from_object(DevConfig)
 from app import views
