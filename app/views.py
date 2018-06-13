af898e40dd1d5df0fe9f772e71fdaae000a45e1a
newtonkiragu <karanunewton4@gmail.com>
2018-06-13 19:51:05 +0300

    displayed movies based on categories

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 #views
 @app.route('/')
@@ -7,8 +8,13 @@ def index():
     '''
     view root page function that returns index and it\'s data
     '''
+
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to the best movie review website online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
@@ -16,4 +22,4 @@ def movie(movie_id):
     view movie page function that returns movies and all it\'s data
     '''
     title = 'Movie - Welcome to the best movie review website online'
-    return render_template('movie.html', title = title, id = movie_id)
+    return render_template('index.html', title = title,popular = popular_movies)
