b8ab87448b38efc62e6e21aee2897e88a0ac9ecd
newtonkiragu <karanunewton4@gmail.com>
2018-06-13 19:52:32 +0300

    refactored movie code

--- app/models/movie.py
+++ app/models/movie.py
@@ -1,6 +1,6 @@
 class Movie:
     '''
-    Movie class to define movie objects
+    Movie class to define Movie Objects
     '''
 
     def __init__(self,id,title,overview,image,vote_average,vote_count):
@@ -8,5 +8,5 @@ class Movie:
         self.title = title
         self.overview = overview
         self.image = 'https://image.tmdb.org/t/p/w500/'+image
-        self.vote_average =  vote_average
+        self.vote_average = vote_average
         self.vote_count = vote_count
