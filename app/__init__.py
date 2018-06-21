f4d45bdad35cc7465ac62b09c6c245fbfebe0c7a
newtonkiragu <karanunewton4@gmail.com>
2018-06-21 17:26:33 +0300

    removed db to host easily

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask 
 from config import config_options
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
 def create_app(config_name):
     #....
     app = Flask(__name__)
 
     bootstrap.init_app(app)
-    db.init_app(app)
+    # db.init_app(app)
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
