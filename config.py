88587b9405e9c481bd631ec32a305dbd8acf6397
newtonkiragu <karanunewton4@gmail.com>
2018-08-07 15:11:06 +0300

    added secret key

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class  Config:
 
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = '6809e9daeffd8c82b4ef74b653ae5ff7'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://monster:manager666@localhost/watchlist'    
+    SECRET_KEY = 'iamasecretkey'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://monster:manager666@localhost/watchlist'
 
 class ProdConfig(Config):
     '''
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export MOVIE_API_KEY='6809e9daeffd8c82b4ef74b653ae5ff7'
-# export SECRET_KEY = 'secret'
+export SECRET_KEY = 'secret'
 
-python3.6 manage.py server 
+python3.6 manage.py server
